fb3303e9d45c0303754592dc0368bf97996e2870
minaaaayk <mina.yaghubi95@gmail.com>
2020-07-18 11:33:10 +0430

    #1 fixed reducer fixed #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { Dispatch } from 'redux';
-import { connect } from 'react-redux';
-import { PostsState,Post } from './store/Posts/types';
-import * as actions from './store/Posts/actions';
+import { Dispatch } from "redux";
+import { connect } from "react-redux";
+import { PostsState, Post } from "./store/Posts/types";
+import * as actions from "./store/Posts/actions";
 
-const mapStateToProps = (State: PostsState) => {
-  return { posts:State.posts };
-}
+const mapStateToProps = (State: {
+  PostReducer: PostsState;
+  /*other reducers*/
+}) => {
+  return { posts: State.PostReducer.posts };
+};
 
 const mapDispatcherToProps = (dispatch: Dispatch<actions.ActionsType>) => {
-    return {
-        getAllPosts: () => dispatch(actions.getAllPosts()),
-        getOnrPost:  (id:number) => dispatch(actions.getOnePost(id)),
-        getPostComments: (postId: number) => dispatch(actions.getPostComments(postId)),
-        DeletePost: (postId:number) => dispatch(actions.DeletePost(postId)),
-        AddPost: (post:Post) => dispatch(actions.AddPost(post))
-    }
-}
-type ReduxType = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatcherToProps>;
+  return {
+    getAllPosts: () => dispatch(actions.getAllPosts()),
+    getOnrPost: (id: number) => dispatch(actions.getOnePost(id)),
+    getPostComments: (postId: number) =>
+      dispatch(actions.getPostComments(postId)),
+    DeletePost: (postId: number) => dispatch(actions.DeletePost(postId)),
+    AddPost: (post: Post) => dispatch(actions.AddPost(post)),
+  };
+};
+type ReduxType = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatcherToProps>;
 
-const App:React.FC<ReduxType> = props => {
+const App: React.FC<ReduxType> = (props) => {
+  console.log("props", props);
+
   return (
     <div className="App">
       <button onClick={() => props.getAllPosts()}>Get All Posts</button>
       <button onClick={() => props.getOnrPost(1)}>Get Post 1</button>
       {console.log(props.posts)}
       <ul>
-        {props.posts? props.posts.map((p:Post) => 
-          (<li key={p.id}> {p.title} </li>)
-        ): ""}
+        {props.posts
+          ? props.posts.map((p: Post) => <li key={p.id}> {p.title} </li>)
+          : ""}
       </ul>
     </div>
   );
-}
+};
 
 export default connect(mapStateToProps, mapDispatcherToProps)(App);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-import { PostReducer } from './Posts/reducer';
-import { apiMiddleware } from '../middleware-api';
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { PostReducer } from "./Posts/reducer";
+import { apiMiddleware } from "../middleware-api";
 export const rootReducer = combineReducers({
-    Posts: PostReducer,
-  });
+  PostReducer: PostReducer,
+  //other reducers...
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
 export const store = createStore(rootReducer, applyMiddleware(apiMiddleware));
